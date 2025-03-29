fa580ac5f6fc09f36c8b0b7b8373af3e953f57eb
Diogo Duarte <diogomiguelduarte@gmail.com>
2025-03-29 20:22:00 +0000

    Update front page

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,20 @@
 import streamlit as st
 
-st.title('🎈 App Name')
+# Function to display the front page
+def front_page():
+    st.title('🏋️ Workout Planner App')
 
-st.write('Hello world!')
+    st.image("images/template.jpg", use_container_width=True)
+
+    st.write("""
+    ## Sample Text
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit."
+    """)
+
+    st.write("""
+    ## Group Members
+    - **Alice Viegas** (20240572)
+    - **Bernardo Faria** (20240579)
+    - **Brandon Seichal** (20240592)
+    - **Diogo Duarte** (20240525)
+    """)
